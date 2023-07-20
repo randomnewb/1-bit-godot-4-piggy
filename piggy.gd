20e23efd3400a9fd2d675928c73137d30ddd11d5
randomnewb <nguy1581@gmail.com>
2023-07-20 15:40:37 -0500

    added animation to pig and apple scene

--- piggy.gd
+++ piggy.gd
@@ -2,7 +2,16 @@ extends Area2D
 
 @export var speed = 75;
 
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if input_vector == Vector2.ZERO:
+		animation_player.play("idle");
+	else:
+		animation_player.play("run");
+		if input_vector.x != 0:
+			sprite_2d.scale.x = sign(input_vector.x);
 	position += input_vector * speed * delta;
