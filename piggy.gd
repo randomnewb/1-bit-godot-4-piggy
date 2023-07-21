3af7797e8567c07ec7a77f7a3c637c7b292c5187
randomnewb <nguy1581@gmail.com>
2023-07-21 00:03:47 -0500

    added feature where piggy increases in size after consuming apple

--- piggy.gd
+++ piggy.gd
@@ -5,7 +5,6 @@ extends Area2D
 @onready var animation_player = $AnimationPlayer
 @onready var sprite_2d = $Sprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if input_vector == Vector2.ZERO:
@@ -15,3 +14,7 @@ func _process(delta):
 		if input_vector.x != 0:
 			sprite_2d.scale.x = sign(input_vector.x);
 	position += input_vector * speed * delta;
+
+func _on_area_entered(area):
+	area.queue_free()
+	scale *= 1.1
