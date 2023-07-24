f317fbe06d9a804e9817ae909eb6c8b59d940594
randomnewb <nguy1581@gmail.com>
2023-07-24 00:13:27 -0500

    on death, explode and create porkchop (game polish)

--- axe.gd
+++ axe.gd
@@ -3,6 +3,9 @@ extends Area2D
 @export var speed = 10;
 @onready var piggy = $"../Piggy"
 
+var PORKCHOP_SCENE = preload("res://porkchop.tscn");
+var EXPLOSION_EFFECT_SCENE = preload("res://explosion_effect.tscn");
+
 func _process(delta):
 	position = position.move_toward(piggy.position, speed * delta);
 	
@@ -14,3 +17,12 @@ func _process(delta):
 func _on_area_entered(area):
 	area.queue_free();
 	queue_free();
+	var world = get_tree().current_scene;
+	
+	var porkchop = PORKCHOP_SCENE.instantiate();
+	world.add_child.call_deferred(porkchop);
+	porkchop.position = global_position;
+
+	var explosion = EXPLOSION_EFFECT_SCENE.instantiate();
+	world.add_child.call_deferred(explosion);
+	explosion.global_position = global_position;
